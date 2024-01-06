7de3577b15cd1c0fbf3953a48f4bd1b8a9745138
Lucas Garcia <lucasgarciainvest@gmail.com>
2024-01-06 18:08:34 -0300

    Subindo novos exercicios

---
+++ Modulo01/Aula08.py
@@ -0,0 +1,8 @@
+n1 = float (input("Digite o Primeiro número: "))
+n2 = int(input("Digite o Segundo número: "))
+
+soma = n1 + n2
+print(f"A primeira variavel é do tipo {type(n1)}\n e o valor da primeira variavel é {n1}")
+print(f"A segunda variavel é do tipo {type(n2)}\n e o valor da segunda variavel é {n2}")
+
+print(f"O resultado final é do tipo primitivo {type(soma)}\n e o valor do resultado {soma}")
---
+++ Modulo01/Aula09.py
@@ -0,0 +1,6 @@
+pera = input ("Digite qualquer coisa: ")
+
+print("Tem espaço", pera.isspace())
+print("Tem Caracter Numerico", pera.isnumeric())
+print("Contem Caracter alfabetico" , pera.isalpha())
+print("Contem caracter alfanumerico", pera.isalnum())
